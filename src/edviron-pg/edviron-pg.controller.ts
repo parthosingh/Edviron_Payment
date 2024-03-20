d77954e25b82acc3d24c95b227bd8526d4aaa525
Jatin <152367689+jatinr-edviron@users.noreply.github.com>
2024-03-20 10:48:56 +0530

    Update edviron-pg.controller.ts

--- src/edviron-pg/edviron-pg.controller.ts
+++ src/edviron-pg/edviron-pg.controller.ts
@@ -42,7 +42,7 @@ export class EdvironPgController {
     res.send(
       `<script type="text/javascript">
                 window.onload = function(){
-                    location.href = "https://dev.pg.edviron.com?session_id=${req.query.session_id}&collect_request_id=${req.query.collect_request_id}&amount=${req.query.amount}${disable_modes}";
+                    location.href = "https://pg.edviron.com?session_id=${req.query.session_id}&collect_request_id=${req.query.collect_request_id}&amount=${req.query.amount}${disable_modes}";
                 }
             </script>`,
     );
