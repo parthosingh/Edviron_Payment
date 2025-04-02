93a9afa000192d32103e68b2c4e66a65d5766feb
Manishv-Edviron <manish.verma@edviron.com>
2025-04-02 12:09:16 +0530

    upi-credit-card-filter

--- src/edviron-pg/edviron-pg.controller.ts
+++ src/edviron-pg/edviron-pg.controller.ts
@@ -1409,12 +1409,19 @@ export class EdvironPgController {
       searchParams,
       isCustomSearch,
       seachFilter,
-      payment_modes,
+      
       isQRCode,
       gateway,
     } = body;
+    let {
+      payment_modes,
+    } = body;
     if (!token) throw new Error('Token not provided');
 
+    if(payment_modes?.includes('upi')){
+      payment_modes = [...payment_modes, 'upi_credit_card']  //debit_card
+    }
+    
     try {
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 10;
@@ -1502,6 +1509,7 @@ export class EdvironPgController {
         collect_id: { $in: orderIds },
       };
 
+      console.log(query, "query")
       if (startDate && endDate) {
         query = {
           ...query,
