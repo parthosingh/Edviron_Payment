fa28d582db6cd443f88755febba131b75b065853
raj0811 <rpbarmaiya@gmail.com>
2025-04-11 14:25:35 +0530

    add capture status

--- dist/check-status/check-status.service.js
+++ dist/check-status/check-status.service.js
@@ -85,6 +85,7 @@ let CheckStatusService = class CheckStatusService {
                 return {
                     ...edvironPgResponse,
                     custom_order_id,
+                    capture_status: collect_req_status.capture_status || 'NA',
                 };
             case collect_request_schema_1.Gateway.EDVIRON_EASEBUZZ:
                 const easebuzzStatus = await this.easebuzzService.statusResponse(collect_request_id.toString(), collectRequest);
--- src/check-status/check-status.service.ts
+++ src/check-status/check-status.service.ts
@@ -91,7 +91,7 @@ export class CheckStatusService {
         return {
           ...edvironPgResponse,
           custom_order_id,
-          // capture_status: 'PENDING',
+          capture_status: collect_req_status.capture_status || 'NA',
         };
 
       case Gateway.EDVIRON_EASEBUZZ:
