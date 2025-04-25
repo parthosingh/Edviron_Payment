7591bc280c97ae26bb4fb5600d6e776392f94534
raj0811 <rpbarmaiya@gmail.com>
2025-04-25 12:16:05 +0530

    add expire

--- dist/check-status/check-status.service.d.ts
+++ dist/check-status/check-status.service.d.ts
@@ -22,23 +22,9 @@ export declare class CheckStatusService {
     private readonly hdfcRazorpay;
     constructor(databaseService: DatabaseService, hdfcService: HdfcService, phonePeService: PhonepeService, edvironPgService: EdvironPgService, ccavenueService: CcavenueService, easebuzzService: EasebuzzService, cashfreeService: CashfreeService, payUService: PayUService, hdfcRazorpay: HdfcRazorpayService);
     checkStatus(collect_request_id: String): Promise<{
-        status: any;
-        amount: number;
-        transaction_amount: number;
-        status_code: number;
-        details: {
-            payment_mode: any;
-            bank_ref: any;
-            payment_methods: {};
-            transaction_time: any;
-        };
-        mode: any;
-        net_amount_debit: any;
-        bank_ref_num: any;
-    } | {
         status: TransactionStatus;
         amount: number;
-    } | {
+    } | "Invalid request" | {
         status: any;
         status_code: number;
         custom_order_id: string | null;
@@ -53,6 +39,20 @@ export declare class CheckStatusService {
             formattedTransactionDate: string;
             order_status: any;
         };
+    } | {
+        status: any;
+        amount: number;
+        transaction_amount: number;
+        status_code: number;
+        details: {
+            payment_mode: any;
+            bank_ref: any;
+            payment_methods: {};
+            transaction_time: any;
+        };
+        mode: any;
+        net_amount_debit: any;
+        bank_ref_num: any;
     } | {
         status: string;
         statusCode: number;
@@ -69,7 +69,7 @@ export declare class CheckStatusService {
             formattedTransactionDate: string;
             order_status: any;
         };
-    } | "Invalid request" | {
+    } | {
         status: string;
         custom_order_id: string;
         amount: number;
@@ -108,6 +108,9 @@ export declare class CheckStatusService {
         details?: undefined;
     } | undefined>;
     checkStatusByOrderId(order_id: String, school_id: string): Promise<{
+        status: TransactionStatus;
+        amount: number;
+    } | "Invalid request" | {
         status: any;
         amount: number;
         transaction_amount: number;
@@ -122,9 +125,6 @@ export declare class CheckStatusService {
         net_amount_debit: any;
         bank_ref_num: any;
     } | {
-        status: TransactionStatus;
-        amount: number;
-    } | "Invalid request" | {
         status: string;
         custom_order_id: string;
         amount: number;
