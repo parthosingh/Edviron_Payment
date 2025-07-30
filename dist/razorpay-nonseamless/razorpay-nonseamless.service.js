d96244408843d790093137c6fdc677a10d542b71
Manishv-Edviron <manish.verma@edviron.com>
2025-07-30 18:08:11 +0530

    update-razorpay-settlement

--- dist/razorpay-nonseamless/razorpay-nonseamless.service.js
+++ dist/razorpay-nonseamless/razorpay-nonseamless.service.js
@@ -431,7 +431,8 @@ let RazorpayNonseamlessService = class RazorpayNonseamlessService {
             }
             const year = date.getFullYear();
             const month = String(date.getMonth() + 1).padStart(2, '0');
-            const day = String(date.getDate()).padStart(2, '0');
+            const day = String(date.getDate() + 1).padStart(2, '0');
+            console.log(year, month, day, 'check');
             const config = {
                 method: 'get',
                 url: `https://api.razorpay.com/v1/settlements/recon/combined?year=${year}&month=${month}&day=${day}&count=${limit}&skip=${skip}`,
